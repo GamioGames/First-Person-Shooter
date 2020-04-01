89131685af2e7755e05c4a90356fc95253824744
antony999k <lv999k@hotmail.com>
2020-04-01 16:18:10 -0600

    :sparkles: Movimiento

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(CharacterController))]
+public class PlayerController : MonoBehaviour
+{
+    public float walkSpeed = 5f;
+    public float runSpeed = 10f;
+    public float jumpHeight = 1.9f;
+    public float gravityScale = -20f;
+
+    Vector3 moveInput = Vector3.zero;
+    CharacterController characterController;
+
+    private void Awake()
+    {
+        characterController = GetComponent<CharacterController>();
+    }
+
+    private void Update()
+    {
+        Move();
+    }
+
+    private void Move()
+    {
+        if (characterController.isGrounded)
+        {
+            moveInput = new Vector3(Input.GetAxis("Horizontal"), 0f, Input.GetAxis("Vertical"));
+            moveInput = Vector3.ClampMagnitude(moveInput, 1f);
+
+            if (Input.GetButton("Sprint"))
+            {
+                moveInput = transform.TransformDirection(moveInput) * runSpeed;
+            }
+            else
+            {
+                moveInput = transform.TransformDirection(moveInput) * walkSpeed;
+            }
+
+            if (Input.GetButtonDown("Jump"))
+            {
+                moveInput.y = Mathf.Sqrt(jumpHeight * -2f * gravityScale);
+            }
+        }
+
+        moveInput.y += gravityScale * Time.deltaTime;
+        characterController.Move(moveInput * Time.deltaTime);
+    }
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+# Curso FPS (First Person Shooter)
