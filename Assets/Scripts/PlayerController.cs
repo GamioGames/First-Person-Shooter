123912ecd976f5b803bf0fc1d4c32240b0b95fc3
antony999k <lv999k@hotmail.com>
2020-04-06 15:24:40 -0500

    :sparkles: Movimiento de camara de personaje

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,12 +5,25 @@ using UnityEngine;
 [RequireComponent(typeof(CharacterController))]
 public class PlayerController : MonoBehaviour
 {
-    public float walkSpeed = 5f;
-    public float runSpeed = 10f;
-    public float jumpHeight = 1.9f;
+    [Header ("References")]
+    public Camera playerCamera;
+
+    [Header ("General")]
     public float gravityScale = -20f;
 
+    [Header ("Movement")]
+    public float walkSpeed = 5f;
+    public float runSpeed = 10f;
+
+    [Header("Rotation")]
+    public float rotationSensibility = 10f;
+
+    [Header("Jump")]
+    public float jumpHeight = 1.9f;
+
+    private float cameraVerticalAngle;
     Vector3 moveInput = Vector3.zero;
+    Vector3 rotationinput = Vector3.zero;
     CharacterController characterController;
 
     private void Awake()
@@ -20,6 +33,7 @@ public class PlayerController : MonoBehaviour
 
     private void Update()
     {
+        Look();
         Move();
     }
 
@@ -47,5 +61,17 @@ public class PlayerController : MonoBehaviour
 
         moveInput.y += gravityScale * Time.deltaTime;
         characterController.Move(moveInput * Time.deltaTime);
+    }
+
+    private void Look()
+    {
+        rotationinput.x = Input.GetAxis("Mouse X") * rotationSensibility * Time.deltaTime;
+        rotationinput.y = Input.GetAxis("Mouse Y") * rotationSensibility * Time.deltaTime;
+
+        cameraVerticalAngle = cameraVerticalAngle + rotationinput.y;
+        cameraVerticalAngle = Mathf.Clamp(cameraVerticalAngle, -70, 70);
+
+        transform.Rotate(Vector3.up * rotationinput.x);
+        playerCamera.transform.localRotation = Quaternion.Euler(-cameraVerticalAngle,0f,0f);
     }
 }
