9bfad52515a506587e41be64bb541dd0cd7b3df1
antony999k <lv999k@hotmail.com>
2020-05-02 11:07:18 -0500

    :sparkles: Disparo: Hoyos de balas

--- Assets/Scripts/WeaponController.cs
+++ Assets/Scripts/WeaponController.cs
@@ -4,13 +4,35 @@ using UnityEngine;
 
 public class WeaponController : MonoBehaviour
 {
+    [Header("General")]
+    public LayerMask hittableLayers;
+    public GameObject bulletHolePrefab;
+
+    [Header("Shoot Paramaters")]
+    public float fireRange = 200;
+
+    private Transform cameraPlayerTransform;
+
     private void Start()
     {
-        
+        cameraPlayerTransform = GameObject.FindGameObjectWithTag("MainCamera").transform;
     }
 
     private void Update()
     {
-        
+        HandleShoot();
+    }
+
+    private void HandleShoot()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            RaycastHit hit;
+            if (Physics.Raycast(cameraPlayerTransform.position, cameraPlayerTransform.forward, out hit, fireRange, hittableLayers))
+            {
+                GameObject bulletHoleClone = Instantiate(bulletHolePrefab, hit.point + hit.normal * 0.001f, Quaternion.LookRotation(hit.normal));
+                Destroy(bulletHoleClone, 4f);
+            } 
+        }
     }
 }
