d4a02ef6d342886c60cfd390ddd255b069a1cd2e
antony999k <lv999k@hotmail.com>
2020-05-24 18:47:00 -0500

    :sparkles: Municion y lógica de recarga

--- Assets/Scripts/WeaponController.cs
+++ Assets/Scripts/WeaponController.cs
@@ -14,11 +14,25 @@ public class WeaponController : MonoBehaviour
     [Header("Shoot Paramaters")]
     public float fireRange = 200;
     public float recoilForce = 4f; //Fuerza de retroceso del arma
+    public float fireRate = 0.6f;
+    public int maxAmmo = 8;
+
+    [Header("Reload Parameters")]
+    public float reloadTime = 1.5f;
+
+    public int currentAmmo { get; private set; }
+
+    private float lastTimeShoot = Mathf.NegativeInfinity;
 
     [Header("Sounds & Visuals")]
     public GameObject flashEffect;
 
     private Transform cameraPlayerTransform;
+
+    private void Awake()
+    {
+        currentAmmo = maxAmmo;
+    }
 
     private void Start()
     {
@@ -27,32 +41,62 @@ public class WeaponController : MonoBehaviour
 
     private void Update()
     {
-        HandleShoot();
+        if (Input.GetButtonDown("Fire1"))
+        {
+            TryShoot();
+        }
+
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            StartCoroutine(Reload());
+        }
 
         transform.localPosition = Vector3.Lerp(transform.localPosition, Vector3.zero, Time.deltaTime * 5f);
     }
 
+    private bool TryShoot()
+    {
+        if (lastTimeShoot + fireRate < Time.time)
+        {
+            if (currentAmmo >= 1)
+            {
+                HandleShoot();
+                currentAmmo -= 1;
+                return true;
+            }
+        }
+
+        return false;
+    }
+
     private void HandleShoot()
     {
-        if (Input.GetButtonDown("Fire1"))
+        GameObject flashClone = Instantiate(flashEffect, weaponMuzzle.position, Quaternion.Euler(weaponMuzzle.forward), transform);
+        Destroy(flashClone, 1f);
+
+        AddRecoil();
+
+        RaycastHit hit;
+        if (Physics.Raycast(cameraPlayerTransform.position, cameraPlayerTransform.forward, out hit, fireRange, hittableLayers))
         {
-            GameObject flashClone = Instantiate(flashEffect, weaponMuzzle.position, Quaternion.Euler(weaponMuzzle.forward), transform);
-            Destroy(flashClone, 1f);
-
-            AddRecoil();
-
-            RaycastHit hit;
-            if (Physics.Raycast(cameraPlayerTransform.position, cameraPlayerTransform.forward, out hit, fireRange, hittableLayers))
-            {
-                GameObject bulletHoleClone = Instantiate(bulletHolePrefab, hit.point + hit.normal * 0.001f, Quaternion.LookRotation(hit.normal));
-                Destroy(bulletHoleClone, 4f);
-            } 
+            GameObject bulletHoleClone = Instantiate(bulletHolePrefab, hit.point + hit.normal * 0.001f, Quaternion.LookRotation(hit.normal));
+            Destroy(bulletHoleClone, 4f);
         }
+
+        lastTimeShoot = Time.time;
     }
 
     private void AddRecoil()
     {
         transform.Rotate(-recoilForce, 0f, 0f);
         transform.position = transform.position - transform.forward * (recoilForce/50f);
+    }
+
+    IEnumerator Reload()
+    {
+        Debug.Log("Recargando...");
+        yield return new WaitForSeconds(reloadTime);
+        currentAmmo = maxAmmo;
+        Debug.Log("Recargada");
     }
 }
